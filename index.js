47d2720a898527707cd10e5b61d6862ee5279bef
jrc452 <jairoreyescalvario@gmail.com>
2025-10-16 14:41:23 +0200

    Update index

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const buttons = document.querySelectorAll('a.btn');
 const iframe = document.getElementById('iframe');
+const mainSection = document.getElementById('mainSection');
+
+mainSection.hidden = true;
 
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
         event.preventDefault();
         const href = button.getAttribute('href');
         iframe.setAttribute('src', href);
+        mainSection.hidden = false;
         iframe.scrollIntoView({ behavior: 'smooth' });
     });
 });
