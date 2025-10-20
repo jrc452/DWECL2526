bdf816b65dc5ae904720add43755a5617a1dcfd9
jrc452 <jairoreyescalvario@gmail.com>
2025-10-20 10:26:52 +0200

    Update t1ej4

--- tema1/t1ej4.js
+++ tema1/t1ej4.js
@@ -1,4 +1,8 @@
 function mostrarImagen() {
     let imagenSeleccionada = document.getElementById("imagenSelect").value;
-    document.getElementById("imagenDiv").innerHTML = '<img src="' + imagenSeleccionada + '" alt="Imagen seleccionada" width="200">';
+    if (imagenSeleccionada !== "") {
+        document.getElementById("imagenDiv").innerHTML = '<img src="' + imagenSeleccionada + '" alt="Imagen seleccionada" width="200">';
+        console.log("Imagen seleccionada:", imagenSeleccionada);
+    } else
+        document.getElementById("imagenDiv").innerHTML = '';
 }
