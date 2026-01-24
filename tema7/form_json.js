5b9bab77e58ff03b7bd361b1e1597b49ed5a3181
jrc452 <jairoreyescalvario@gmail.com>
2026-01-24 16:39:43 +0100

    Update form_json

--- tema7/form_json.js
+++ tema7/form_json.js
@@ -39,12 +39,43 @@ btnClear.addEventListener('click', () => {
 });
 
 btnSave.addEventListener('click', () => {
-    if (alumnos.length > 1)
-        guardarComoJson();
+    if (alumnos.length > 0) guardarComoJson();
 });
 
 btnLoad.addEventListener('click', () => {
-    cargarJson();
+    document.getElementById("fileinput").click();
+});
+
+// --- 5. CARGAR JSON ---
+document.getElementById("fileinput").addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    alumnos = [];
+    guardarYSincronizar();
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        try {
+            const data = JSON.parse(e.target.result);
+            data.forEach(d => {
+                const jsonData = {};
+                Object.entries(d).forEach((h, c) => {
+                    if (c > 0) {
+                        if (c === 1) jsonData.nombre = h[1];
+                        if (c === 2) jsonData.apellidos = h[1];
+                        if (c === 3) jsonData.edad = h[1];
+                        if (c === 4) jsonData.nota = h[1];
+                        if (c === 5) jsonData.movil = h[1];
+                    }
+                });
+                alumnos.push(jsonData);
+            });
+            guardarYSincronizar(); // Guardamos y pintamos
+            form.reset();
+        } catch (error) { }
+    };
+    reader.readAsText(file);
 });
 
 
@@ -60,33 +91,38 @@ function renderizar() {
     cuerpo.innerHTML = "";
     alumnos.forEach((alum, i) => {
         const tr = document.createElement('tr');
-        tr.innerHTML = `<td>${i + 1}</td><td>${alum.nombre}</td>`;
-        tr.innerHTML = `<td>${i + 1}</td><td>${alum.apellidos}</td>`;
-        tr.innerHTML = `<td>${i + 1}</td><td>${alum.edad}</td>`;
         if (alum.nota >= 5)
-            tr.innerHTML = `<td>${i + 1}</td><td>${alum.nota}</td>`;
+            tr.innerHTML = `<td>${i + 1}</td>
+                            <td>${alum.nombre}</td>
+                            <td>${alum.apellidos}</td>
+                            <td>${alum.edad}</td>
+                            <td>${alum.nota}</td>
+                            <td>${alum.movil}</td>`;
         else
-            tr.innerHTML = `<td>${i + 1}</td><td><span style="color: red;">${alum.nota}</span></td>`;
-        tr.innerHTML = `<td>${i + 1}</td><td>${alum.movil}</td>`;
+            tr.innerHTML = `<td>${i + 1}</td>
+                            <td>${alum.nombre}</td>
+                            <td>${alum.apellidos}</td>
+                            <td>${alum.edad}</td>
+                            <td style="color: red;">${alum.nota}</td>
+                            <td>${alum.movil}</td>`;
         cuerpo.appendChild(tr);
     });
 }
 
 // --- 4. GUARDAR COMO JSON ---
 function guardarComoJson() {
-    const tabla = document.getElementById("tabla");
-    const headers = [], data = [];
+    const table = document.getElementById("tabla");
+    const headers = Array.from(table.querySelectorAll('thead th')).map(th => th.textContent.trim());
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    const data = rows.map(row => {
+        const cells = Array.from(row.querySelectorAll('td'));
+        let rowObject = {};
 
-    tabla.querySelectorAll('th').forEach(th => {
-        headers.push(th.textContent.trim());
-    });
-
-    tabla.querySelectorAll('tbody tr').forEach(tr => {
-        const rdata = {};
-        tabla.querySelectorAll('td').forEach((c, i) => {
-            tr[headers[i]] = c.textContent.trim();
+        headers.forEach((h, i) => {
+            rowObject[h] = cells[i] ? cells[i].textContent.trim() : null;
         });
-        data.push(rdata);
+
+        return rowObject;
     });
 
     const json2string = JSON.stringify(data, null, 2);
@@ -95,40 +131,7 @@ function guardarComoJson() {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'listado_con_persistencia.json';
+    a.download = "listado_con_persistencia.json";
     a.click();
     URL.revokeObjectURL(url);
-}
-
-// --- 4. CARGAR JSON ---
-function cargarJson() {
-    const data = JSON.parse(e.target.result);
-    const file = e.target.files = [0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        try {
-            data = JSON.parse(e.target.result);
-        } catch (e) { }
-    }
-
-    cuerpo.innerHTML = "";
-
-    data.forEach(r => {
-        const tr = document.createElement('tr');
-        const v = Object.values(r);
-        v.forEach(c => {
-            const td = document.createElement('td');
-            td.textContent = c;
-            if (c == v[4] && alum.nota < 5)
-                td.style = "color: red";
-            tr.appendChild(td);
-        });
-        cuerpo.appendChild(tr);
-    });
-
-    alumnos.push(nuevo);
-    guardarYSincronizar(); // Guardamos y pintamos
-    form.reset();
 }
